d7262f860caa5c2310093b1132480634fe846e19
MTLY-1610 <larrouy.m@me.com>
2020-04-07 15:22:25 +0200

    -

---
+++ server.js
@@ -0,0 +1,16 @@
+//imports
+const express = require('espress');
+
+//instantiate server
+const server = express();
+
+//Configure routes
+server.get('/', function (req, res){
+    res.setHeader('Content-Type','text/html');
+    res.status(200).send('<h1>Hello sick world</h1>');
+});
+
+//Launch server
+server.listen(3000, function() {
+    console.log('listening');
+});
