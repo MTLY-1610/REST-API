dc0718d37f5e02a3d27bfff9b5f4d37ac423ec4f
MTLY-1610 <larrouy.m@me.com>
2020-04-20 17:12:43 +0200

    beautify

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 //user List
 
 const users = [];
-for(let i = 0; i <10; i++){
+for (let i = 0; i < 10; i++) {
     users.push({
 
-        firstname:faker.name.firstName(),
+        firstname: faker.name.firstName(),
         lastname: faker.name.lastName(),
         jobtitle: faker.name.jobTitle(),
         email: faker.internet.email(),
@@ -20,71 +20,73 @@ for(let i = 0; i <10; i++){
 
 //Vesion
 const versionApi = '/api/v1';
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // parse application/json
 app.use(bodyParser.json());
 
 
-
 //GET /api/v1/users
-app.get (`${versionApi}/users`, (req, res) => {
+app.get(`${versionApi}/users`, (req, res) => {
     res.send({
 
         data: users
-        
+
     })
 });
 
 
 //GET /api/v1/users/:id
-app.get (`${versionApi}/users/:id`, (req, res) => {
-    const id = req.params.id -1;
+app.get(`${versionApi}/users/:id`, (req, res) => {
+    const id = req.params.id - 1;
 
     res.send({
 
         data: users[id] || null
-        
+
     })
 });
 
 //POST /api/v1/users
-app.post (`${versionApi}/users`, (req, res) => {
+app.post(`${versionApi}/users`, (req, res) => {
     const data = req.body;
 
     users.push(data);
 
     res.send({
         index: users.length,
-        data: users [users.length - 1]
-        
+        data: users[users.length - 1]
+
     })
 });
 
 
 //PUT /api/v1/users/:id
-app.put (`${versionApi}/users/:id`, (req, res) => {
-    const id = req.params.id - 1;
-    const data = req.body;
+app.put(`${versionApi}/users/:id`, (req, res) => {
+        const id = req.params.id - 1;
+        const data = req.body;
 
-    users[id] = Object.assign(users[id], data);
+        users[id] = Object.assign(users[id], data);
 
-    res.send({
-        index: users.length,
-        data: users [id]
-        
+        res.send({
+            index: users.length,
+            data: users[id]
+
+        })
+    }),
+
+    //DELETE /api/v1/users/:id
+    app.delete(`${versionApi}/users/:id`, (req, res) => {
+        const id = req.params.id - 1;
+
+        users.splice(id, 1);
+        res.send(200);
     })
-}),
-
-//DELETE /api/v1/users/:id
-app.delete(`${versionApi}/users/:id`, (req, res) => {
-    const id = req.params .id - 1;
-
-    users.splice (id, 1);
-    res.send(200);
-})
 
 
 
-app.listen(3000, () => console.log(''));
+app.listen(3000, () => console.log('listening'));
