89213bea6a02389b8d85590d52939d47a4c7d9d1
MTLY-1610 <larrouy.m@me.com>
2020-04-20 14:19:20 +0200

    setup faker

--- server.js
+++ server.js
@@ -1,16 +1,54 @@
-//imports
 const express = require('express');
+const faker = require('faker');
+const app = express();
 
-//instantiate server
-const server = express();
 
-//Configure routes
-server.get('/', function (req, res){
-    res.setHeader('Content-Type','text/html');
-    res.status(200).send('<h1>Hello sick world</h1>');
+//user List
+
+const users = [];
+for(let i = 0; i <10; i++){
+    users.push({
+
+        firstname:faker.name.firstName(),
+        lastname: faker.name.lastName(),
+        jobtitle: faker.name.jobTitle(),
+        email: faker.internet.email(),
+    })
+}
+
+
+//Vesion
+const versionApi = '/api/v1';
+ 
+
+//GET /api/v1/users
+app.get (`${versionApi}/users`, (req, res) => {
+    res.json({
+
+        data: users
+        
+    })
 });
 
-//Launch server
-server.listen(3000, function() {
-    console.log('listening');
+
+//GET /api/v1/users/:id
+app.get (`${versionApi}/users/:id`, (req, res) => {
+    const id = req.params.id -1;
+
+    res.json({
+        
+        data: undefined
+        
+    })
 });
+
+//POST /api/v1/users
+
+//PUT /api/v1/users/:id
+
+//DELETE /api/v1/users/:id
+
+
+
+
+app.listen(3000, () => console.log('rouston'));
